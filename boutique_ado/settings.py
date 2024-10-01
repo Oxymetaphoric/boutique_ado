e2875e9d6abb03edc421014e003a765688a5ca45
Paul Tapping <zombieplayground@gmail.com>
2024-10-01 23:28:46 +0100

    BUILD: secure checkout form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     # Optional -- requires install using `django-allauth[socialaccount]`.
     'allauth.socialaccount',
+    'crispy_forms',
+
     'home',
     'products',
     'bag',
@@ -62,6 +64,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -76,8 +80,14 @@ TEMPLATES = [
                  'django.template.context_processors.request', #req. by allAuth
                  'django.contrib.auth.context_processors.auth',
                  'django.contrib.messages.context_processors.messages',
+                 'django.template.context_processors.media',
                  'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+                
+                ],
         },
     },
 ]
