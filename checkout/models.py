098b018f3fb3574dc45816af0afa0cf5672a44de
Paul Tapping <zombieplayground@gmail.com>
2024-10-08 14:47:21 +0100

    BUILD: checkout success template and views

--- checkout/models.py
+++ checkout/models.py
@@ -24,7 +24,7 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
-        return uuid.uuid4.hex.upper()
+        return uuid.uuid4().hex.upper()
 
     def update_total(self):
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
@@ -47,7 +47,7 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
 
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitem')
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XK)
     quantity = models.IntegerField(null=False, blank=False, default=0)
